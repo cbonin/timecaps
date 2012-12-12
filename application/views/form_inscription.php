227e79c6d23590dced5491b9c63168765dc1d868
Procureur <mc.flurry@hotmail.fr>
2012-12-12 17:02:12 +0100

    views/formulaire.php avec son css

---
+++ application/views/form_inscription.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title></title>
+	<meta name="description" content="">
+  <link rel="stylesheet" href="../../assets/css/form.css">
+</head>
+<body>
+  
+    <form>
+      <label for="prenom">Votre prenom</label>
+      <input type="text" id="prenom" name="prenom" required >
+      <label for="nom">Votre nom</label>
+      <input type="text" id="prenom" name="nom" required >
+      <label for="email">Votre email</label>
+      <input type="email" id="email" name="email" required >
+      <label for="password">Votre Mot de passe</label>
+      <input type="password" id="password" name="password" required >
+      <label for="passwordConf">Confirmer mot de passe</label>
+      <input type="password" id="passwordConf" name="passwordConf" required >
+    </form>
+
+</body>
+</html>

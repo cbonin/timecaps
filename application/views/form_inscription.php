e889aa200c1092a2307b79ba1f291bf6d45c7329
Procureur <mc.flurry@hotmail.fr>
2012-12-12 18:34:53 +0100

    form_inscription formatté

--- application/views/form_inscription.php
+++ application/views/form_inscription.php
@@ -9,19 +9,17 @@
 </head>
 <body>
 
-  <?php echo validation_errors(); ?>
-  
-    <form action="<?php echo base_url(); ?>userController/signUp" method="POST">
+    <form method="POST" action="<?php base_url();?>userController/signUp">
       <label for="prenom">Votre prenom</label>
-      <input type="text" id="prenom" name="prenom" required >
+      <input type="text" id="prenom" name="prenom" value="Charles" required >
       <label for="nom">Votre nom</label>
-      <input type="text" id="prenom" name="nom" required >
+      <input type="text" id="prenom" name="nom" value="ZBOUBI" required >
       <label for="email">Votre email</label>
-      <input type="email" id="email" name="email" required >
+      <input type="email" id="email" name="email" value="c@gmail.com" required >
       <label for="password">Votre Mot de passe</label>
-      <input type="password" id="password" name="password" required >
+      <input type="password" id="password" name="password" value="zboub" required >
       <label for="passwordConf">Confirmer mot de passe</label>
-      <input type="password" id="passwordConf" name="passwordConf" required >
+      <input type="password" id="passwordConf" name="passwordConf" value="zboub" required >
       <input type="submit" />
     </form>
 

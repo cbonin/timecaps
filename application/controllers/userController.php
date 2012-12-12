3eabf4d98ff233332d1afed30cc879a5494cde6a
BastienPenalba <bastien.penalba@gmail.com>
2012-12-12 17:01:43 +0100

    zboub

--- application/controllers/userController.php
+++ application/controllers/userController.php
@@ -20,7 +20,8 @@ class User_controller extends CI_Controller
 		$this->form_validation->set_rules('prenom', 'Prénom', 'trim|required|xss_clean|max_length[50]');
 		$this->form_validation->set_rules('nom', 'Nom', 'trim|required|xss_clean|max_length[50]');
 		$this->form_validation->set_rules('email', 'Adresse mail', 'trim|required|xss_clean|max_length[100]|valid_email|is_unique[user.email]');
-		$this->form_validation->set_rules('password', 'Mot de passe', 'trim|required|xss_clean|valid_email|sha1');
+		$this->form_validation->set_rules('password', 'Mot de passe', 'trim|required|xss_clean|valid_email|md5|matches[passwordConf]');
+		$this->form_validation->set_rules('passwordConf', 'confirmation mot de passe', 'trim|required|xss_clean|valid_email|md5');
 
 		if ($this->form_validation->run() == FALSE)
 		{

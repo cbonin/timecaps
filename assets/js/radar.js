e2733ab1597e782252d5d5e31aa174796d3d3754
Procureur <mc.flurry@hotmail.fr>
2012-12-13 17:20:23 +0100

    radar avec date

--- assets/js/radar.js
+++ assets/js/radar.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var today = new Date;
+   var today = new Date;
     today = today.getFullYear()+''+(today.getMonth()+1)+''+today.getDate();
 
     function matchPosition(){
@@ -13,11 +13,11 @@ $(document).ready(function () {
         function checkPosition(position){
             var latitude = position.coords.latitude;
             var longitude = position.coords.longitude;
-            console.log('boiteX '+boiteX);
-            console.log('boiteY '+boiteY);
             console.log('PosX '+latitude);
             console.log('PosY '+longitude);
-            if((position.coords.latitude - boiteX < 0.2) && (position.coords.longitude - boiteY < 0.2) && today >= boiteDate){
+            console.log('boiteX '+boiteX);
+            console.log('boiteY '+boiteY);
+            if((position.coords.latitude - boiteX < 0.2) && (position.coords.longitude - boiteY < 0.2) && (today >= boiteDate)){
                 console.log('zbooub victory');
                 document.getElementById('position').innerHTML = position.coords.longitude + ' ' + position.coords.latitude;
                 document.getElementById('unlocker').disabled = false;

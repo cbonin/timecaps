fbe4bc7536cebf35b1eb35fa36c2f966b57d06be
Procureur <mc.flurry@hotmail.fr>
2012-12-13 16:29:17 +0100

    d

--- application/views/front/header.php
+++ application/views/front/header.php
@@ -3,7 +3,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-	<title><php? echo $title; ?></title>
+	<title><?php echo $title; ?></title>
 	<meta name="description" content="">
   <link rel="stylesheet" href="../../assets/css/front.css">
 </head>

4c83a6d45e2498c4e334f8391ebd6feee26ce705
Procureur <mc.flurry@hotmail.fr>
2012-12-20 18:12:21 +0100

    caca

--- application/views/back/unlocker.php
+++ application/views/back/unlocker.php
@@ -1,10 +1,8 @@
-<div id="container">
-	<section id="connexion">
-		<h1>Ouvrir une boite sponsorisée</h1>
-		<form method="POST" action="<?php echo base_url(); ?>boiteController/unlockBrand">
-			<label for="code">Code :</label>
-			<input type="text" id="code" name="code" />
-			<input type="submit" class="button" value="Connexion" name=""/>
-		</form>
-	</section>
-</div>
+<section id="connexion">
+	<h1>Ouvrir une boite sponsorisée</h1>
+	<form method="POST" action="<?php echo base_url(); ?>boiteController/unlockBrand">
+		<label for="code">Code :</label>
+		<input type="text" id="code" name="code" />
+		<input type="submit" class="button" value="Connexion" name=""/>
+	</form>
+</section>
